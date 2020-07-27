0b3c0ff57fef191cb2587cb52f3790e97b0b19ef
cezary-gawronski <idetepowiec@gmail.com>
2020-07-27 22:07:16 +0200

    added navbar and mobile menu

--- script/script.js
+++ script/script.js
@@ -1,1 +1,27 @@
 
+(() => {
+    const mobileButton = document.querySelector(".js-navigation__mobile-button");
+    const mobileMenu = document.querySelector(".js-navigationMobile");
+
+    const toggleClass = () => {
+        mobileMenu.classList.toggle("navigation__list--isActiveMobile");
+    };
+
+    mobileButton.addEventListener("click", toggleClass);
+    mobileMenu.addEventListener("click", toggleClass);
+
+})();
+
+
+// (() => {
+//     const button = document.querySelector(".js-main-navigation__mobile-button");
+//     const menu = document.querySelector(".js-main-navigation__menu");
+
+//     const toggleClass = () => {
+//         menu.classList.toggle("main-navigation__menu--open");
+//     };
+
+//     button.addEventListener("click", toggleClass);
+//     menu.addEventListener("click", toggleClass);
+
+// })()
